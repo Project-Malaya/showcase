c6175090b550d40ebb6d1713777393f4a7fafe62
airtorres <komsaira2015@gmail.com>
2023-07-05 23:22:59 +0800

    Install Chakra UI and add to root App

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,30 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button, Flex } from '@chakra-ui/react'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <Flex justifyContent="center">
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
+      </Flex>
       <h1>Welcome to Showcase</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <p className="card">
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-      </div>
+      </p>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
